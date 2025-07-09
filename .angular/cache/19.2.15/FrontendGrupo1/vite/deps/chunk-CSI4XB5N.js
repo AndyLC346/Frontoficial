059ea8388557e23f2f8bcbd9a4672bf690598e77
Andy Jheremy Lazaro Curia <149219743+AndyLC346@users.noreply.github.com>
2025-07-09 13:33:54 -0500

    fetch

---
+++ .angular/cache/19.2.15/FrontendGrupo1/vite/deps/chunk-CSI4XB5N.js
@@ -0,0 +1,41 @@
+import {
+  MatRipple
+} from "./chunk-MHRHIQWF.js";
+import {
+  MatCommonModule
+} from "./chunk-GMSTBHD3.js";
+import {
+  NgModule,
+  setClassMetadata,
+  ɵɵdefineInjector,
+  ɵɵdefineNgModule
+} from "./chunk-LBCYZSVY.js";
+
+// node_modules/@angular/material/fesm2022/index-SYVYjXwK.mjs
+var MatRippleModule = class _MatRippleModule {
+  static ɵfac = function MatRippleModule_Factory(__ngFactoryType__) {
+    return new (__ngFactoryType__ || _MatRippleModule)();
+  };
+  static ɵmod = ɵɵdefineNgModule({
+    type: _MatRippleModule,
+    imports: [MatCommonModule, MatRipple],
+    exports: [MatRipple, MatCommonModule]
+  });
+  static ɵinj = ɵɵdefineInjector({
+    imports: [MatCommonModule, MatCommonModule]
+  });
+};
+(() => {
+  (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(MatRippleModule, [{
+    type: NgModule,
+    args: [{
+      imports: [MatCommonModule, MatRipple],
+      exports: [MatRipple, MatCommonModule]
+    }]
+  }], null, null);
+})();
+
+export {
+  MatRippleModule
+};
+//# sourceMappingURL=chunk-CSI4XB5N.js.map
---
+++ .angular/cache/19.2.15/FrontendGrupo1/vite/deps_ssr/@angular_material_button.js
@@ -0,0 +1,45 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-MEJ53FWK.js";
+import "./chunk-IUMKQCPR.js";
+import "./chunk-K2RBOZZJ.js";
+import "./chunk-PW5DN7PE.js";
+import "./chunk-VAT3HF6J.js";
+import "./chunk-BABSEUN5.js";
+import "./chunk-C5HDTQAM.js";
+import "./chunk-JME5XKN5.js";
+import "./chunk-EF6L3WMP.js";
+import "./chunk-UUTP3235.js";
+import "./chunk-5ICBFHDG.js";
+import "./chunk-KMXVIEOJ.js";
+import "./chunk-XZIY4MOL.js";
+import "./chunk-POUHVWWY.js";
+import "./chunk-SWIVHK54.js";
+import "./chunk-AQYIT73X.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
